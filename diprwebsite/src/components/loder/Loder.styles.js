2581c40247134d60225ef3b7eaeb6d4729c9714a
chetanv18~ <chetan@gully2global.com>
2025-02-18 10:45:17 +0530

    new

--- diprwebsite/src/components/loder/Loder.styles.js
+++ diprwebsite/src/components/loder/Loder.styles.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
-// Styled container for the loader
 export const LoaderContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh; /* Full viewport height */
+  height: 100vh; 
   background-color: rgba(255, 255, 255, 0.8);
 `;
