508aeee8b4f82129ae940b84895c2abc81eac46e
chetanv18~ <chetan@gully2global.com>
2025-02-19 12:04:41 +0530

    new

--- diprwebsite/src/components/categorytab/CategoryTab.jsx
+++ diprwebsite/src/components/categorytab/CategoryTab.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
-import {  FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import {
   TabContainer,
   TabsWrapper,
@@ -10,7 +10,7 @@ import {
 } from "../categorytab/CategoryTab.styles";
 import ProfileImage from "../../assets/Profile.png";
 import { FontSizeContext } from "../../context/FontSizeProvider";
- 
+
 const tabs = [
   { name: "All", path: "/" },
   { name: "Latest News", path: "/latestdata" },
@@ -19,60 +19,64 @@ const tabs = [
   { name: "About us", path: "/aboutuspage" },
   { name: "Contact us", path: "/contactus" },
 ];
- 
+
 const CategoryTab = () => {
   const location = useLocation();
   const [activeTab, setActiveTab] = useState(location.pathname);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const { fontSize } = useContext(FontSizeContext);
-  
- 
+  const { fontSize } = useContext(FontSizeContext);
+
   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
- 
-  // Close the menu after clicking a tab if screen width > 480px
+
   const handleTabClick = (path) => {
     setActiveTab(path);
     if (window.innerWidth <= 480) {
-      setIsMenuOpen(false); 
+      setIsMenuOpen(false);
     }
   };
- 
-  // Listen for window resize to update the menu state
+
+  useEffect(() => {
+    setActiveTab(location.pathname);
+  }, [location.pathname]);
+
+  // Close menu on larger screens
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth > 480) {
-        setIsMenuOpen(false); // Close menu on larger screens
+        setIsMenuOpen(false);
       }
     };
     window.addEventListener("resize", handleResize);
- 
+
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
- 
+
   return (
-    <TabContainer style={{ fontSize: `${fontSize}%` }} >
+    <TabContainer style={{ fontSize: `${fontSize}%` }}>
       <HamburgerMenu onClick={toggleMenu}>
         {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
       </HamburgerMenu>
       <TabsWrapper style={{ fontSize: `${fontSize}%` }} isMenuOpen={isMenuOpen}>
         {tabs.map(({ name, path }) => (
-          <Link 
+          <Link
             key={name}
             to={path}
             onClick={() => handleTabClick(path)}
             style={{ textDecoration: "none" }}
           >
-            <TabItem style={{ fontSize: `${fontSize}%` }} active={activeTab === path}>{name}</TabItem>
+            <TabItem style={{ fontSize: `${fontSize}%` }} active={activeTab === path}>
+              {name}
+            </TabItem>
           </Link>
         ))}
       </TabsWrapper>
-            <Link to="/profile" style={{ marginLeft: "auto" , marginRight: "4%" }}>
-            <ProfileIcon src={ProfileImage} alt="User Profile" />
+      <Link to="/profile" style={{ marginLeft: "auto", marginRight: "4%" }}>
+        <ProfileIcon src={ProfileImage} alt="User Profile" />
       </Link>
     </TabContainer>
   );
 };
- 
+
 export default CategoryTab;
