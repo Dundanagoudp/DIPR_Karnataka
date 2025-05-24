efde38f08faa36b6019ae983376d4f09617ccb4d
chetanv18~ <chetan@gully2global.com>
2025-05-24 11:02:56 +0530

    new

--- diprwebsite/src/modules/pages/Home/Home.jsx
+++ diprwebsite/src/modules/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ import {
 } from "../../pages/Home/Home.styles";
 import Loader from "../../../components/loder/Loder";
 import AllNewsData from "../../components/allnewssection2/AllNewsData";
-import Videos from "../../components/homevideosection/Videos";
+import Videos from "../../../modules/components/homevideosection/Videos";
 import Magzines2 from "../../components/magzinemodal/MagzinewithModal";
 import ShortsCarousel from "../../components/homevideosection/ShortsCarousel";
 
