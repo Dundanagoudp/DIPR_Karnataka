37ada15aa5daf4b80b10066d4411cb1d99d18d53
chetanv18~ <chetan@gully2global.com>
2025-10-15 13:23:33 +0530

    march of karnataka magzine page updated

---
+++ diprwebsite/src/components-newdesigns/pages/marchofkarnatakview/MarchKarnatakIdpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MarchKarnatakIdview from '../../components/magzines-singleview/Modules/MarchKarnatakIdview'
+
+export default function MarchKarnatakIdpage() {
+  return (
+    <div>
+      <MarchKarnatakIdview/>
+    </div>
+  )
+}
+
