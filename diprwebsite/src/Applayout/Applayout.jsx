7f70c06cfe177ed5ff1dc7f9f8deb013f235a76b
chetanv18~ <chetan@gully2global.com>
2025-10-15 14:24:04 +0530

    applayout page updated

--- diprwebsite/src/Applayout/Applayout.jsx
+++ diprwebsite/src/Applayout/Applayout.jsx
@@ -1,9 +1,4 @@
 import { Outlet } from "react-router-dom";
-// // import NavBar from "../components/navbar/NavBar";
-// // import CategoryTab from "../components/categorytab/CategoryTab";
-import Footer from "../components/Footer/Footer";
-// // import ToolBar from "../components/ToolBar/ToolBar";
-// import LoadingProgressBar from "../components/Progressloader/ProgressLoading";
 import Header from "../components-newdesigns/layout/Header/Header";
 import HeaderTab from "../components-newdesigns/layout/headertabs/HeaderTab";
 import LanguageNavbar from "../components-newdesigns/layout/languagetranslation/LanguageNavbar";
@@ -13,22 +8,16 @@ import TrendingBar from "../components-newdesigns/layout/trendingbar/TrendingBar
 
 const Applayout = () => {
   return (
-    // <LoadingProgressBar> 
       <div role="application" aria-label="DIPR Website">
         <Header />
         <LanguageNavbar />
         <HeaderTab />
         <TrendingBar />
-        {/* <CategoryTab /> */}
-        {/* <NavBar /> */}
-        {/* <ToolBar /> */}
         <main role="main" aria-label="Main content">
         <Outlet />
         </main>
-        {/* <Footer/> */}
         <SiteFooter />
       </div>
-    // </LoadingProgressBar>
   );
 };
 
