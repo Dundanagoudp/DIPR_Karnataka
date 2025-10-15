ab0a0263c1cf9e2896cf4367cffce5fbe3e7507c
chetanv18~ <chetan@gully2global.com>
2025-10-15 15:00:19 +0530

    appjsx file updated

--- diprwebsite/src/App.jsx
+++ diprwebsite/src/App.jsx
@@ -4,14 +4,14 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./theme/GlobalStyle";
 import theme from "./theme/Theme";
 import Login from "./screens/Login/Login";
-import Otp from "./screens/otp/Otp";
+// import Otp from "./screens/otp/Otp";
 import Applayout from "./Applayout/Applayout";
-import Home from "./modules/pages/Home/Home";
-import LatestNews from "./modules/pages/Latestnews/LatestNews";
-import MagaZinepages from "./modules/pages/magazinepage/MagaZinepage";
-import AboutUspage from "./modules/pages/aboutuspage/AboutUspage";
-import Exclusive from "./modules/pages/exclusivepage/Exclusive";
-import ContactUs from "./modules/pages/ContactUspage/ContactUs";
+// import Home from "./modules/pages/Home/Home";
+// import LatestNews from "./modules/pages/Latestnews/LatestNews";
+// import MagaZinepages from "./modules/pages/magazinepage/MagaZinepage";
+// import AboutUspage from "./modules/pages/aboutuspage/AboutUspage";
+// import Exclusive from "./modules/pages/exclusivepage/Exclusive";
+// import ContactUs from "./modules/pages/ContactUspage/ContactUs";
 import CopyrightPolicy from "./components/WebsitePolicies/CopyrightPolicy/CopyrightPolicy";
 import HyperlinkingPolicy from "./components/WebsitePolicies/HyperlinkingPolicy/HyperlinkingPolicy";
 import SecurityPolicy from "./components/WebsitePolicies/SecurityPolicy/SecurityPolicy";
@@ -19,17 +19,17 @@ import Guidelines from "./components/WebsitePolicies/Guidelines/Guidelines";
 import TermsAndConditions from "./components/WebsitePolicies/TermsAndConditions/TermsAndConditions";
 import PrivacyPolicy from "./components/WebsitePolicies/PrivacyPolicy/PrivacyPolicy";
 import Help from "./components/WebsitePolicies/Help/Help";
-import LatestData from "./modules/pages/Latest/LatestData";
+// import LatestData from "./modules/pages/Latest/LatestData";
 import Profile from "./components/Profile/Profile";
 import FontSizeProvider from "./context/FontSizeProvider";
 import { LanguageProvider } from "./context/LanguageContext";
 import Cookies from "js-cookie";
 import { endSession } from "./services/auth/LoginApi"; 
-import Gallery from "./modules/pages/dynamicGallery/Gallery";
-import LatestNews1 from "./modules/pages/Recommednews/RecomMended";
+// import Gallery from "./modules/pages/dynamicGallery/Gallery";
+// import LatestNews1 from "./modules/pages/Recommednews/RecomMended";
 import Magazineview from "./modules/components/Magazinepagecom/magzineidpage/Magazineview";
 import { ToastProvider } from "./context/ToastContext";
-import SignUppage from "./screens/signuppage/SignUp";
+// import SignUppage from "./screens/signuppage/SignUp";
 import StateNews from "./components-newdesigns/pages/state/StateNews";
 import DistrictNewspage from "./components-newdesigns/pages/districtNewspage/DistrictNewspage";
 import SpecialNews from "./components-newdesigns/pages/specialnewspage/SpecialNews";
@@ -38,7 +38,7 @@ import Marchofkarnataka from "./components-newdesigns/pages/marchofkarnataka/Mar
 import NewsId from "./components-newdesigns/pages/newsid/NewsId";
 import VarthaJanapadasection from "./components-newdesigns/components/varthaJanapada/VarthaJanapadasection";
 import MagazinesVartha from "./components-newdesigns/pages/varthamagazines/MagazinesVartha";
-import MarchofKarnataka from "./components-newdesigns/pages/marchofkarnataka/Marchofkarnataka";
+// import MarchofKarnataka from "./components-newdesigns/pages/marchofkarnataka/Marchofkarnataka";
 import MarchofKarnatakMagzine from "./components-newdesigns/pages/marchofkarnatakmagzine/MarchofKarnatak";
 import MagzineIdpage from "./components-newdesigns/pages/magzineid/MagzineIdpage";
 import MarchKarnatakIdpage from "./components-newdesigns/pages/marchofkarnatakview/MarchKarnatakIdpage";
@@ -81,13 +81,14 @@ const App = () => {
                 <Route path="/" element={<Applayout />}>
                   {/* <Route index element={<Home />} /> */}
                   <Route index element={<VarthaJanapadasection />} />
-                  <Route path="/news/:id" element={<LatestNews />} />
+                  {/* <Route path="/news/:id" element={<LatestNews />} /> */}
                   {/* <Route path="/newsdetails/:id" element={<LatestNews1 />} /> */}
-                  <Route path="/magazinepages" element={<MagaZinepages />} />
-                  <Route path="/Gallery" element={<Exclusive />} />
-                  <Route path="/aboutuspage" element={<AboutUspage />} />
-                  <Route path="/contactus" element={<ContactUs />} />
-                  <Route path="/latestnews" element={<LatestData />} />
+                  {/* <Route path="/magazinepages" element={<MagaZinepages />} /> */}
+                  {/* <Route path="/Gallery" element={<Exclusive />} /> */}
+                  {/* <Route path="/aboutuspage" element={<AboutUspage />} /> */}
+                  {/* <Route path="/contactus" element={<ContactUs />} /> */}
+                  {/* <Route path="/latestnews" element={<LatestData />} /> */}
+                  {/* <Route path="/gallerysection" element={<Gallery />} /> */}
                   <Route path="/copyright-policy" element={<CopyrightPolicy />} />
                   <Route path="/hyperlinking-policy" element={<HyperlinkingPolicy />} />
                   <Route path="/security-policy" element={<SecurityPolicy />} />
@@ -96,7 +97,6 @@ const App = () => {
                   <Route path="/privacy-policy" element={<PrivacyPolicy />} />
                   <Route path="/help" element={<Help />} />
                   <Route path="/profile" element={<Profile />} />
-                  <Route path="/gallerysection" element={<Gallery />} />
                   <Route path="/magazine/:id" element={<Magazineview />} />
                   <Route path="/state" element={<StateNews />} />
                   <Route path="/district" element={<DistrictNewspage />} />
